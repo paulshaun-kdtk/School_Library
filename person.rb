5bc4af197604ab0b8a9be189b93f17da9b4ccbeb
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-10-26 13:03:34 +0200

    -

--- person.rb
+++ person.rb
@@ -66,4 +66,4 @@ puts capitalized_person.correct_name
 # Output: Maximilianus
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
-# Output: Maximilian...
+# Output: Maximilian..
